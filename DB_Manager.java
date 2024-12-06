5ccae8926d1d8db63c221cdfca0910524be04432
Marc Dähne <daehne.marc@gmail.com>
2024-12-07 12:57:23 +1300

    made user and pw more general

--- DB_Manager.java
+++ DB_Manager.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class DB_Manager {
 
     private static final String DB_URL = "jdbc:oracle:thin:@//localhost:1521/XEPDB1";
-    private static final String DB_USER = "inka";
-    private static final String DB_PASSWORD = "testPW";
+    private static final String DB_USER = "USER";
+    private static final String DB_PASSWORD = "PW";
 
     // klappt irgendwie nicht :(
     public static Connection ConnectDB() {
